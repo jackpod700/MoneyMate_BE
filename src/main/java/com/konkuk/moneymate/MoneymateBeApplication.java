49613810a5b79d3b92dd26212d5abe935983e2a9
BOOK-E238ST6C93\jackp <jackpod7002@konkuk.ac.kr>
2025-03-18 15:18:52 +0900

    feat : project init

---
+++ src/main/java/com/konkuk/moneymate/MoneymateBeApplication.java
@@ -0,0 +1,13 @@
+package com.konkuk.moneymate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneymateBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneymateBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/konkuk/moneymate/MoneymateBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konkuk.moneymate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneymateBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
