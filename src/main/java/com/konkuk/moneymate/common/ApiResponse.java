9bb3dd94acc34712d65c9d8558df9ac51abbb9a1
BOOK-E238ST6C93\jackp <jackpod7002@konkuk.ac.kr>
2025-05-22 20:14:26 +0900

    feat: ApiResponseMessage 내용추가

--- src/main/java/com/konkuk/moneymate/common/ApiResponse.java
+++ src/main/java/com/konkuk/moneymate/common/ApiResponse.java
@@ -1,11 +1,9 @@
 package com.konkuk.moneymate.common;
 
-import com.konkuk.moneymate.bankAccount.dto.BankAccountDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @Setter
--- src/main/java/com/konkuk/moneymate/common/ApiResponseMessage.java
+++ src/main/java/com/konkuk/moneymate/common/ApiResponseMessage.java
@@ -1,7 +1,13 @@
 package com.konkuk.moneymate.common;
 
 public enum ApiResponseMessage {
-    ACCOUNT_REGISTER_SUCCESS("계좌등록 성공");
+    //성공시
+    ACCOUNT_REGISTER_SUCCESS("계좌등록 성공"),
+    ACCOUNT_QUERY_SUCCESS("계좌조회 성공"),
+    TRANSACTION_QUERY_SUCCESS("거래내역조회 성공"),
+
+    //실패시
+    WRONG_FORMAT("잘못된 형식");
 
     private String message;
 
