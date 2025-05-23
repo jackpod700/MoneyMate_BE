557cb7f71eda59aa9d0b97d8da34c05c1ffe6a82
BOOK-E238ST6C93\jackp <jackpod7002@konkuk.ac.kr>
2025-05-23 15:17:56 +0900

    feat: TransactionDto 추가

---
+++ src/main/java/com/konkuk/moneymate/activities/dto/TransactionDto.java
@@ -0,0 +1,18 @@
+package com.konkuk.moneymate.activities.dto;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class TransactionDto {
+    private LocalDate trDate;
+    private LocalTime trTime;
+    private int trOut;
+    private int trIn;
+    private int trAfterBalance;
+    private String trDest;
+
+}
