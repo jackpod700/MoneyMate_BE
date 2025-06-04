9d78e376d1085113905afe0e9304bb1062e2524b
BOOK-E238ST6C93\jackp <jackpod7002@konkuk.ac.kr>
2025-06-04 15:28:19 +0900

    fix: 더미데이터 수정

--- src/main/resources/sql/insert_bankaccount.sql
+++ src/main/resources/sql/insert_bankaccount.sql
@@ -18,7 +18,6 @@ INSERT INTO bank_account (uid, user_uid, account_number, name, bank, current_bal
 (UNHEX(REPLACE('3d1fa2cb-0c87-42a1-b829-3c51a6de3222', '-', '')), UNHEX(REPLACE('c9bf9e57-1685-4c89-bafb-ff5af830be8a', '-', '')), '7894561230', '신한 주거래통장', '088', 3800000, '입출금'),
 (UNHEX(REPLACE('d501b80f-4f2e-4a2e-98e9-f8d4ad7e4222', '-', '')), UNHEX(REPLACE('c9bf9e57-1685-4c89-bafb-ff5af830be8a', '-', '')), '8796453120', '국민 적금통장', '004', 10000000, '예적금'),
 (UNHEX(REPLACE('a110d2f0-2a4a-4c1c-981a-75e4b3f5e433', '-', '')), UNHEX(REPLACE('c9bf9e57-1685-4c89-bafb-ff5af830be8a', '-', '')), '7539514563', 'NH투자증권 계좌', '247', 4300000, '증권'),
-(UNHEX(REPLACE('8c91f5d0-0c47-429f-a7db-1e9f24b8e611', '-', '')), UNHEX(REPLACE('a2cb3298-937e-463b-8476-7f0e5fd7e2f1', '-', '')), '3214567890', '우리입출금통장', '020', 1200000, '입출금'),
 (UNHEX(REPLACE('9e8a3a9f-5a64-4c82-b131-c7ebf7baf612', '-', '')), UNHEX(REPLACE('a2cb3298-937e-463b-8476-7f0e5fd7e2f1', '-', '')), '3141592653589', '카카오페이통장', '090', 3000000, '입출금'),
 (UNHEX(REPLACE('dfe1c4af-6075-4f4f-bcd3-7a16f0d3f613', '-', '')), UNHEX(REPLACE('a2cb3298-937e-463b-8476-7f0e5fd7e2f1', '-', '')), '1597534682', '삼성증권 계좌', '240', 8000000, '증권'),
 (UNHEX(REPLACE('a8d0f1c3-5b8d-4e3f-a7aa-fb3b4c6b6414', '-', '')), UNHEX(REPLACE('e4d1a9fa-6e60-48db-865a-5cf0b1b07e7e', '-', '')), '4561237890', '카카오뱅크통장', '090', 5500000, '입출금'),
