d66c402f50337f56fc0a66e67b9f20a505d8f0a7
hyyy3 <25hanayuki@gmail.com>
2025-06-08 07:44:23 +0900

    commit-06-08-3 jar 빌드 상태에서 데이터 삽입 가능하도록 수정 - properties jdbc 설정값 스프링으로 주입

--- src/main/java/com/konkuk/moneymate/DataInsert.java
+++ src/main/java/com/konkuk/moneymate/DataInsert.java
@@ -1,11 +1,12 @@
 package com.konkuk.moneymate;
 
-
 import com.konkuk.moneymate.activities.entity.*;
 import com.konkuk.moneymate.activities.repository.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import java.io.FileInputStream;
@@ -13,8 +14,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-
-import java.util.Properties;
 
 import java.io.*;
 import java.sql.*;
@@ -35,20 +34,17 @@ public class DataInsert implements CommandLineRunner {
     private final TransactionRepository transactionRepository;
     private final MonthlyAssetHistoryRepository monthlyAssetHistoryRepository;
 
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
+
+    @Value("${jdbc.username}")
+    private String username;
+
+    @Value("${jdbc.password}")
+    private String password;
+
     @Override
     public void run(String ... args) throws Exception {
-        Properties props = new Properties();
-        try (InputStream input = new FileInputStream("src/main/resources/application.properties")) {
-            props.load(input);
-            log.info("application.properties 읽기 성공");
-        } catch (IOException e) {
-            log.error("application.properties 읽기 실패: {}", e.getMessage());
-            return;
-        }
-
-        String jdbcUrl = props.getProperty("jdbc.url");
-        String username = props.getProperty("jdbc.username");
-        String password = props.getProperty("jdbc.password");
 
         if (jdbcUrl == null || username == null || password == null) {
             log.error("application.properties 설정을 확인하세요");
@@ -61,16 +57,15 @@ public class DataInsert implements CommandLineRunner {
             String currentDir = System.getProperty("user.dir");
             log.info("current dir: {}", currentDir);
 
-            executeSqlScript(conn, "src/main/resources/sql/insert_users.sql");
-            executeSqlScript(conn, "src/main/resources/sql/insert_bankaccount.sql");
-            executeSqlScript(conn, "src/main/resources/sql/insert_asset.sql");
-            executeSqlScript(conn, "src/main/resources/sql/insert_transaction.sql");
+            executeSqlScript(conn, "sql/insert_users.sql");
+            executeSqlScript(conn, "sql/insert_bankaccount.sql");
+            executeSqlScript(conn, "sql/insert_asset.sql");
+            executeSqlScript(conn, "sql/insert_transaction.sql");
 
             log.info("All SQL execution success");
         } catch (SQLException e) {
             log.error("DB connection failed: {}", e.getMessage());
         }
-
 
         /**
          * Sample data 삽입 부분 2
@@ -109,7 +104,8 @@ public class DataInsert implements CommandLineRunner {
 
     private static void executeSqlScript(Connection conn, String filePath) {
         log.info("실행 중: {}", filePath);
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        try (InputStream is = new ClassPathResource(filePath).getInputStream();
+             BufferedReader reader = new BufferedReader(new InputStreamReader(is));
              Statement stmt = conn.createStatement()) {
             StringBuilder sqlBuilder = new StringBuilder();
             String line;
@@ -133,3 +129,23 @@ public class DataInsert implements CommandLineRunner {
     }
 }
 
+
+
+/*
+Properties props = new Properties();
+        try (InputStream input = new FileInputStream("src/main/resources/application.properties")) {
+            props.load(input);
+            log.info("application.properties 읽기 성공");
+        } catch (IOException e) {
+            log.error("application.properties 읽기 실패: {}", e.getMessage());
+            return;
+        }
+ */
+// String jdbcUrl = props.getProperty("jdbc.url");
+// String username = props.getProperty("jdbc.username");
+// String password = props.getProperty("jdbc.password");
+
+/*
+try (BufferedReader reader = new BufferedReader(new FileReader(filePath));
+             Statement stmt = conn.createStatement()) {
+ */
