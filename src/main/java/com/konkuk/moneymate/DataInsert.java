4c3204dac6a64b24cc37d4cfefd8442f0cfc1480
hyyy3 <25hanayuki@gmail.com>
2025-06-08 07:02:59 +0900

    commit-06-08-1 자산 데이터 등록, 애플리케이션 클래스 수정

--- src/main/java/com/konkuk/moneymate/DataInsert.java
+++ src/main/java/com/konkuk/moneymate/DataInsert.java
@@ -20,6 +20,10 @@ import java.io.*;
 import java.sql.*;
 import java.util.Properties;
 
+/**
+ * <h3>DataInsert : 데이터 삽입 클래스 </h3>
+ * <p>Spring bean에 포함되어 자동으로 실행됩니다</p>
+ */
 @Component
 @Slf4j
 @RequiredArgsConstructor
@@ -59,12 +63,18 @@ public class DataInsert implements CommandLineRunner {
 
             executeSqlScript(conn, "src/main/resources/sql/insert_users.sql");
             executeSqlScript(conn, "src/main/resources/sql/insert_bankaccount.sql");
+            executeSqlScript(conn, "src/main/resources/sql/insert_asset.sql");
             executeSqlScript(conn, "src/main/resources/sql/insert_transaction.sql");
 
             log.info("All SQL execution success");
         } catch (SQLException e) {
             log.error("DB connection failed: {}", e.getMessage());
         }
+
+
+        /**
+         * Sample data 삽입 부분 2
+         */
 
         User user1 = new User("user_id", "user", "$2a$12$ZNsd2Al0w2hjPeq9icxl.Oydgv3.hbQ3UlsWCqkJfmXeD8t6Wq9sm", "010-0000-0000", LocalDate.of(2000, 1, 1));
         User user2 = new User("admin_id", "admin", "$2a$12$T05/pakINgU7nUagCdInRe8rC6xPK1sHuhxlUuSIQPENfAogqeGpG", "010-1111-2222", LocalDate.of(2001, 12, 31));
@@ -76,9 +86,9 @@ public class DataInsert implements CommandLineRunner {
         Transaction transaction2 = new Transaction(bankAccount1, bankAccount2.getAccountNumber(), 20000, null, "이체", LocalDateTime.of(2025, 5, 24, 10, 32), 100000L);
         Transaction transaction3 = new Transaction(bankAccount1, bankAccount2.getAccountNumber(), 30000, null, "이체", LocalDateTime.of(2025, 5, 24, 10, 33), 50000L);
 
-        Asset asset1 = new Asset(user1, 3700000000L, "반포자이 84A", "real-estate");
-        Asset asset2 = new Asset(user1, 50000000L, "제네시스 G80/2G", "car");
-        Asset asset3 = new Asset(user2, 2147483637L, "e편한세상광진그랜드파크 105A", "real-estate");
+        Asset asset1 = new Asset(user1, 3700000000L, "반포자이 84A", "부동산");
+        Asset asset2 = new Asset(user1, 50000000L, "제네시스 G80/2G---", "투자");
+        Asset asset3 = new Asset(user2, 2147483637L, "e편한세상광진그랜드파크 105A", "부동산");
 
         MonthlyAssetHistory monthlyAssetHistory1 = new MonthlyAssetHistory(user1, LocalDate.of(2025, 5, 1), 100000L, 0L, 50000L);
         MonthlyAssetHistory monthlyAssetHistory2 = new MonthlyAssetHistory(user2, LocalDate.of(2025, 5, 1), 50000L, 30000L, 0L);
--- src/main/resources/sql/insert_asset.sql
+++ src/main/resources/sql/insert_asset.sql
@@ -1,12 +1,33 @@
 
 INSERT INTO asset (asset_uid, user_uid, name, type, price) VALUES
-(UNHEX(REPLACE('7e8a4d2a-1a58-4d2e-a61e-3ff6c6e1a111', '-', '')), UNHEX(REPLACE('f47ac10b-58cc-4372-a567-0e02b2c3d479', '-', '')), '반포자이 84A', 'real-estate', 12050000),
-(UNHEX(REPLACE('b349fa3e-2b63-48a5-9672-f8913b5e5111', '-', '')), UNHEX(REPLACE('f47ac10b-58cc-4372-a567-0e02b2c3d479', '-', '')), '제네시스 ', 'car', 6500000),
-(UNHEX(REPLACE('e9f1c8d7-7654-43e2-9a2e-8adf2bbfe111', '-', '')), UNHEX(REPLACE('f47ac10b-58cc-4372-a567-0e02b2c3d479', '-', '')), '미래에셋증권', '230', 5000000),
-(UNHEX(REPLACE('3d1fa2cb-0c87-42a1-b829-3c51a6de3222', '-', '')), UNHEX(REPLACE('c9bf9e57-1685-4c89-bafb-ff5af830be8a', '-', '')), '신한 주거래통장', '088', 3800000),
-(UNHEX(REPLACE('d501b80f-4f2e-4a2e-98e9-f8d4ad7e4222', '-', '')), UNHEX(REPLACE('c9bf9e57-1685-4c89-bafb-ff5af830be8a', '-', '')), '국민 적금통장', '004', 10000000),
-(UNHEX(REPLACE('a110d2f0-2a4a-4c1c-981a-75e4b3f5e433', '-', '')), UNHEX(REPLACE('c9bf9e57-1685-4c89-bafb-ff5af830be8a', '-', '')), 'NH투자증권 계좌', '247', 4300000),
-(UNHEX(REPLACE('9e8a3a9f-5a64-4c82-b131-c7ebf7baf612', '-', '')), UNHEX(REPLACE('a2cb3298-937e-463b-8476-7f0e5fd7e2f1', '-', '')), '카카오페이통장', '090', 3000000),
-(UNHEX(REPLACE('dfe1c4af-6075-4f4f-bcd3-7a16f0d3f613', '-', '')), UNHEX(REPLACE('a2cb3298-937e-463b-8476-7f0e5fd7e2f1', '-', '')), '삼성증권 계좌', '240', 8000000),
-(UNHEX(REPLACE('a8d0f1c3-5b8d-4e3f-a7aa-fb3b4c6b6414', '-', '')), UNHEX(REPLACE('e4d1a9fa-6e60-48db-865a-5cf0b1b07e7e', '-', '')), '카카오뱅크통장', '090', 5500000),
-(UNHEX(REPLACE('b0c1a8e5-3b1a-4cb6-8bba-67d4e4c6d415', '-', '')), UNHEX(REPLACE('e4d1a9fa-6e60-48db-865a-5cf0b1b07e7e', '-', '')), '하나 적금통장', '081', 7800000);
+(UNHEX(REPLACE('5cea9ac9-a31e-4002-a958-75d6b1b9b43b', '-', '')), UNHEX(REPLACE('f47ac10b-58cc-4372-a567-0e02b2c3d479', '-', '')), '헬리오시티 84H', '부동산', 2500000000),
+(UNHEX(REPLACE('78b4bd85-ff35-435e-aa1d-37db8ada0385', '-', '')), UNHEX(REPLACE('f47ac10b-58cc-4372-a567-0e02b2c3d479', '-', '')), '미국 국채 10년(US10YT=RR)', '투자', 158611200),
+(UNHEX(REPLACE('0ab36f39-79ab-4b1f-9f7b-ba46394ae63d', '-', '')), UNHEX(REPLACE('f47ac10b-58cc-4372-a567-0e02b2c3d479', '-', '')), '가상화폐자산', '투자', 7910420),
+(UNHEX(REPLACE('34a7dbb6-d0f5-4b04-afd9-0b3e3cb36fcd', '-', '')), UNHEX(REPLACE('c9bf9e57-1685-4c89-bafb-ff5af830be8a', '-', '')), '더샵스타시티 122D', '부동산', 1800000000),
+(UNHEX(REPLACE('6f062886-33a6-4c84-8b96-bd1bb3089546', '-', '')), UNHEX(REPLACE('c9bf9e57-1685-4c89-bafb-ff5af830be8a', '-', '')), '에셋플러스코리아리치투게더연금자투자신탁1주식CPe', '투자', 22510000),
+(UNHEX(REPLACE('33c077b7-36d2-4f4c-a100-40c7d35d5135', '-', '')), UNHEX(REPLACE('a2cb3298-937e-463b-8476-7f0e5fd7e2f1', '-', '')), '창동주공3단지 59', '부동산', 610000000),
+(UNHEX(REPLACE('0b585a86-3e71-4c12-a0a2-e58ce9657bbd', '-', '')), UNHEX(REPLACE('a2cb3298-937e-463b-8476-7f0e5fd7e2f1', '-', '')), '실물 금 1kg', '투자', 147310000),
+(UNHEX(REPLACE('7a0ded01-e6b7-425f-97f2-934672e33c66', '-', '')), UNHEX(REPLACE('e4d1a9fa-6e60-48db-865a-5cf0b1b07e7e', '-', '')), '마포래미안푸르지오 84B', '부동산', 2100000000),
+(UNHEX(REPLACE('00925dd4-d6e1-4d2c-9440-dea4ae30ea6f', '-', '')), UNHEX(REPLACE('e4d1a9fa-6e60-48db-865a-5cf0b1b07e7e', '-', '')), '가상화폐자산', '투자', 54020000 );
+
+
+
+
+
+-- user_uid
+-- f47ac10b-58cc-4372-a567-0e02b2c3d479
+-- c9bf9e57-1685-4c89-bafb-ff5af830be8a
+-- a2cb3298-937e-463b-8476-7f0e5fd7e2f1
+-- e4d1a9fa-6e60-48db-865a-5cf0b1b07e7e
+
+
+-- asset_uid
+-- 5cea9ac9-a31e-4002-a958-75d6b1b9b43b
+-- 78b4bd85-ff35-435e-aa1d-37db8ada0385
+-- 34a7dbb6-d0f5-4b04-afd9-0b3e3cb36fcd
+-- 6f062886-33a6-4c84-8b96-bd1bb3089546
+-- 33c077b7-36d2-4f4c-a100-40c7d35d5135
+-- 0b585a86-3e71-4c12-a0a2-e58ce9657bbd
+-- 7a0ded01-e6b7-425f-97f2-934672e33c66
+-- 00925dd4-d6e1-4d2c-9440-dea4ae30ea6f
+-- 0ab36f39-79ab-4b1f-9f7b-ba46394ae63d
