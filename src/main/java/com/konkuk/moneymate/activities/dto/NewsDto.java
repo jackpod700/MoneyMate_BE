0e0a2043b6af4b1d506e85aaf0ee96f9f89444f7
BOOK-E238ST6C93\jackp <jackpod7002@konkuk.ac.kr>
2025-07-17 22:50:17 +0900

    feat: 뉴스 전체/상세 조회 구현
    [Added Components]
    Dependency : org.jsoup
    Controller : NewsController.java
    Service : NewsService.java
    DTO : NewsDto.java
    ENUM : NewsCategoryCode.java, NewsPublisher.java
    
    [Modified Components]
    ENUM :  ApiResponseMessage.java

---
+++ src/main/java/com/konkuk/moneymate/activities/dto/NewsDto.java
@@ -0,0 +1,16 @@
+package com.konkuk.moneymate.activities.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class NewsDto {
+    private String title;
+    private String link;
+    private String description;
+    private String publisher;
+    private String category;
+    private String pubDate;
+    private String author;
+}
