9620af92351709c80c040abab859ff6f39e31586
BOOK-E238ST6C93\jackp <jackpod7002@konkuk.ac.kr>
2025-08-11 19:37:35 +0900

    feat: 보유주식 조회 기능 구현
    controller, service 메소드 추가: AssetController, AssetService
    DTO추가: StockHoldingDto, StockHoldingResponseDto
    Repository추가: AccountStockRepository
    static메소드 추가: StockPriceApiClient
    기타: DataInsert 삭제

---
+++ src/main/java/com/konkuk/moneymate/activities/dto/StockHoldingResponseDto.java
@@ -0,0 +1,17 @@
+package com.konkuk.moneymate.activities.dto;
+
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class StockHoldingResponseDto {
+    // /asset/stock 의 응답 DTO
+    private String accountName;
+    private String stockName;
+    private String ticker;
+    private String quantity;
+    private String totalPrice; // 필드명을 'totalPrice'로 변경
+    private String profit;
+}
