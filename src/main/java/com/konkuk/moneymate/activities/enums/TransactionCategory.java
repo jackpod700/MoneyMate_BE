58d1928c0dd86580e05b8a2061ac03a3707adfcb
hyyy3 <25hanayuki@gmail.com>
2025-08-18 03:50:50 +0900

    transaction data test commit 08-18-1

--- src/main/java/com/konkuk/moneymate/activities/enums/TransactionCategory.java
+++ src/main/java/com/konkuk/moneymate/activities/enums/TransactionCategory.java
@@ -6,7 +6,7 @@ public enum TransactionCategory {
     /**
      * 수입
      */
-    SALARY("월급"),
+    SALARY("근로소득"),
     REFUND("환불"),
     INTEREST("이자수입"),
     DIVIDEND("배당수입"),
@@ -23,31 +23,38 @@ public enum TransactionCategory {
     LOAN_REPAYMENT("대출상환"),
     TRANSPORT("교통비"),
     SHOPPING("쇼핑"),
+    CONVENIENCE("생활"),
+    MART("마트"),
     COMMUNICATION("통신비"),
-    MEAL("식비"), // 기타 식비
-    //DELIVERY("배달음식"), // 카테고리 소분류로 할 수도 있고 MEAL로 통합할 수도 있음
+    BOOKS("서적"),
+    DINING("음식점"),
+    DELIVERY("배달음식"),
     CONVENIENCE_STORE("편의점"),
     HOUSING("주거"),  // 월세, 관리비
     HEALTHCARE("의료"), // 병원, 약국
     EDUCATION("교육"),  // 학원, 강의
+    TUITION("학비"),
     ENTERTAINMENT("문화생활"), // 영화, 뮤지컬, 전시, 박물관
     SUBSCRIPTION("구독서비스"), // 정기 구독 서비스
+    ACCOMMODATION("숙박"),
     PET("반려동물"),
     CAR("자동차"),
-    COFFEE("카페"),
+    CAFE("카페"),
     BEAUTY("미용/뷰티"),
     CLOTHING("의류/패션"),
     INSURANCE("보험"),
     DONATION("기부"),  // 후원
     PAYMENT("간편결제"),
+    GIFT("선물"),
+    ONLINE_MALL("온라인쇼핑"),
 
     /**
      * 은행
      */
-    TRANSFER("이체"),
+    TRANSFER("이체"), // 출금 전용
     INVESTMENT("투자"),    // 투자상품에
-    DEPOSIT("입금"),
-    WITHDRAWAL("출금"),
+    DEPOSIT("입금"), // 입금 전용
+    WITHDRAWAL("출금"), // 이체 말고 현금 인출하는것
     SAVINGS("예적금"),
     BUSINESS("사업"),
     CARD_PAYMENT("카드대금"),   // 월별 카드대금
